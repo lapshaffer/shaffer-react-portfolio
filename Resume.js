a37fd5fd484223fde496fc7a839609a98539ca32
Lisa Shaffer <lapshaffer@gmail.com>
2021-10-26 21:52:39 -0400

    Create Resume.js

---
+++ Resume.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import resume from '../../assets/ShafferResume- 09_19_2021.pdf'
+
+export default function Resume() {
+<li>
+    <p><a href={resume} target="_blank" rel="noopener noreferrer">Resume</a></p>
+</li>
+}
