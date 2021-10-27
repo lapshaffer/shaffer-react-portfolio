b3491cca35e77f454e97d2357ccccc219485c81a
Lisa Shaffer <lapshaffer@gmail.com>
2021-10-26 21:51:34 -0400

    Create About.js

---
+++ About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import aboutImg from '../../assets/LisaCNLCrop2.png';
+
+export default function About() {
+<div className="page">
+    <div className="mobile-display">
+            <h2 id="about-me">About Me</h2>
+                <img id="img-about-me" src={aboutImg} alt="Lisa, a short, causcasian, blonde person, from the chest up. They are on stage, wearing a Hawaiian shirt, and holding up their hand in the shape of an 'L' on their forehead."></img>
+                <p>My name is Lisa Shaffer, I use they/them pronouns, and I'm based in West Chester, PA. I'm a young professional in the web development field looking to grow further in my skills and apply myself to interesting and innovative projects. I think of coding as a fun, puzzle-solving experience!
+                <br />
+                <br />
+                I'm currently learning to code through the UPenn and Trilogy Full-Stack Flex Bootcamp. At this point in the boot camp, I have developed strong skills in HTML, CSS and JavaScript. I have experience using a variety of development tools and dependencies, including, but not limited to, Node.js, Express.js, Bootstrap, Handlebars, jQuery, MySQL and Sequelize. I look forward to gaining more experience in development and building more responsive, engaging applications.
+                <br />
+                <br />
+                In my free time, I enjoy playing video games and tabletop games with my friends, going on hikes with my fiance, Charley, and playing with our cat, Matthew! I am passionate about LGBTQ+ advocacy and sustainability in the fashion industry. I have a background in environmental biology research, as well as theatre, from my college years. I have experience in acting, directing, stage management, playwriting and improvisational comedy.</p>
+        </div>
+    </div>
+};
