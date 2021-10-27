1ad29739ac01f331e47854eb86bcd486debdeb17
Lisa Shaffer <lapshaffer@gmail.com>
2021-10-26 21:52:23 -0400

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Contact() {
+<div>
+            <h2 id="contact-me">Contact Me</h2>
+                <ul>
+                    <li>
+                        <p>lapshaffer@gmail.com</p>
+                    </li>
+                    <li>
+                        <p><a href="https://www.linkedin.com/in/lisa-shaffer-b29066198/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                    </li>
+                    <li>
+                        <p><a href="https://github.com/lapshaffer" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+                    </li>
+                </ul>
+        </div>
+}
